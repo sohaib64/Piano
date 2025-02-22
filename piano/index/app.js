78f9f1b92e709a349d5555cf048a2e1c1629a3ce
sohaib64 <154500875+sohaib64@users.noreply.github.com>
2025-02-22 21:47:02 +0500

    Add files via upload

---
+++ piano/index/app.js
@@ -0,0 +1,230 @@
+var music1 = new Audio();
+music1.src = "music/music_1.wav";
+
+var music2 = new Audio();
+music2.src = "music/music_2.wav";
+
+var music3 = new Audio();
+music3.src = "music/music_3.wav";
+
+var music4  =new Audio();
+music4.src = "music/music_4.wav";
+
+var music5 = new Audio();
+music5.src = "music/music_5.wav";
+
+var music6 = new Audio();
+music6.src = "music/music_6.wav";
+
+var music7 = new Audio();
+music7.src = "music/music_7.wav";
+
+var music8 = new Audio();
+music8.src = "music/music_8.wav";
+
+var music9 = new Audio();
+music9.src = "music/music_9.wav";
+
+var music10 = new Audio();
+music10.src = "music/music_10.wav";
+
+var music11 = new Audio();
+music11.src = "music/music_11.wav";
+
+var music12 = new Audio();
+music12.src = "music/music_12.wav";
+
+function Audio1() {
+    music2.pause();
+    music3.pause();
+    music4.pause();
+    music5.pause();
+    music6.pause();
+    music7.pause();
+    music8.pause();
+    music9.pause();
+    music10.pause();
+    music11.pause();
+    music12.pause();
+    music1.play();
+}
+
+function Audio2() {
+    music1.pause();
+    music3.pause();
+    music4.pause();
+    music5.pause();
+    music6.pause();
+    music7.pause();
+    music8.pause();
+    music9.pause();
+    music10.pause();
+    music11.pause();
+    music12.pause();
+    music2.play();
+}
+
+function Audio3() {
+    music1.pause();
+    music2.pause();
+    music4.pause();
+    music5.pause();
+    music6.pause();
+    music7.pause();
+    music8.pause();
+    music9.pause();
+    music10.pause();
+    music11.pause();
+    music12.pause();
+    music3.play();
+}
+
+function Audio4() {
+    music1.pause();
+    music2.pause();
+    music3.pause();
+    music5.pause();
+    music6.pause();
+    music7.pause();
+    music8.pause();
+    music9.pause();
+    music10.pause();
+    music11.pause();
+    music12.pause();
+    music4.play();
+}
+
+function Audio5() {
+    music1.pause();
+    music2.pause();
+    music3.pause();
+    music4.pause();
+    music6.pause();
+    music7.pause();
+    music8.pause();
+    music9.pause();
+    music10.pause();
+    music11.pause();
+    music12.pause();
+    music5.play();
+}
+
+function Audio6() {
+    music1.pause();
+    music2.pause();
+    music3.pause();
+    music4.pause();
+    music5.pause();
+    music7.pause();
+    music8.pause();
+    music9.pause();
+    music10.pause();
+    music11.pause();
+    music12.pause();
+    music6.play();
+}
+
+function Audio7() {
+    music1.pause();
+    music2.pause();
+    music3.pause();
+    music4.pause();
+    music5.pause();
+    music6.pause();
+    music8.pause();
+    music9.pause();
+    music10.pause();
+    music11.pause();
+    music12.pause();
+    music7.play();
+}
+
+function Audio8() {
+    music1.pause();
+    music2.pause();
+    music3.pause();
+    music4.pause();
+    music5.pause();
+    music6.pause();
+    music7.pause();
+    music9.pause();
+    music10.pause();
+    music11.pause();
+    music12.pause();
+    music8.play();
+}
+
+function Audio9() {
+    music1.pause();
+    music2.pause();
+    music3.pause();
+    music4.pause();
+    music5.pause();
+    music6.pause();
+    music7.pause();
+    music8.pause();
+    music10.pause();
+    music11.pause();
+    music12.pause();
+    music9.play();
+}
+
+function Audio10() {
+    music1.pause();
+    music2.pause();
+    music3.pause();
+    music4.pause();
+    music5.pause();
+    music6.pause();
+    music7.pause();
+    music8.pause();
+    music10.pause();
+    music11.pause();
+    music12.pause();
+    music9.play();
+}
+
+function Audio10() {
+    music1.pause();
+    music2.pause();
+    music3.pause();
+    music4.pause();
+    music5.pause();
+    music6.pause();
+    music7.pause();
+    music8.pause();
+    music9.pause();
+    music11.pause();
+    music12.pause();
+    music10.play();
+}
+
+function Audio11() {
+    music1.pause();
+    music2.pause();
+    music3.pause();
+    music4.pause();
+    music5.pause();
+    music6.pause();
+    music7.pause();
+    music8.pause();
+    music9.pause();
+    music10.pause();
+    music12.pause();
+    music11.play();
+}
+
+function Audio12() {
+    music1.pause();
+    music2.pause();
+    music3.pause();
+    music4.pause();
+    music5.pause();
+    music6.pause();
+    music7.pause();
+    music8.pause();
+    music9.pause();
+    music10.pause();
+    music11.pause();
+    music12.play();
+}
